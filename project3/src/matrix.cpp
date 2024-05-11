dfceb48c054dc75c44280ea70735c4ede086a054
shawntsai0312 <shawntsai0312@gmail.com>
2024-05-12 02:19:36 +0800

    code fix

--- project3/src/matrix.cpp
+++ project3/src/matrix.cpp
@@ -1,204 +1,267 @@
-#include<iostream>
-#include<iomanip>
-#include<vector>
-#include<cmath>
-#include"matrix.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include "matrix.h"
 using namespace std;
 
-Matrix::Matrix(int col,int row){
-	cout<<"input the matrix"<<endl;
-	for(int i=0;i<col;i++){
-		vector <double> temp;
-		for(int j=0;j<row;j++){
+Matrix::Matrix(int col, int row)
+{
+	cout << "input the matrix" << endl;
+	for (int i = 0; i < col; i++)
+	{
+		vector<double> temp;
+		for (int j = 0; j < row; j++)
+		{
 			double a;
-			cin>>a;
+			cin >> a;
 			temp.push_back(a);
 		}
 		data.push_back(temp);
 	}
 }
 
-void Matrix::setup(){
-	cout<<"input m and n in order to construct a m*n matrix"<<endl;
-	int col,row;
-	cin>>col>>row;
-	cout<<"input the matrix"<<endl;
-	for(int i=0;i<col;i++){
-		vector <double> temp;
-		for(int j=0;j<row;j++){
+void Matrix::setup()
+{
+	cout << "input m and n in order to construct a m*n matrix" << endl;
+	int col, row;
+	cin >> col >> row;
+	cout << "input the matrix" << endl;
+	for (int i = 0; i < col; i++)
+	{
+		vector<double> temp;
+		for (int j = 0; j < row; j++)
+		{
 			double a;
-			cin>>a;
+			cin >> a;
 			temp.push_back(a);
 		}
 		data.push_back(temp);
 	}
 }
 
-void Matrix::print(){
-	for(int i=0;i<getColumn();i++){
-		for(int j=0;j<getRow();j++){
-			cout<<setw(6)<<fixed<<setprecision(2)<<data[i][j];
+void Matrix::print()
+{
+	for (int i = 0; i < getColumn(); i++)
+	{
+		for (int j = 0; j < getRow(); j++)
+		{
+			cout << setw(6) << fixed << setprecision(2) << data[i][j];
 		}
-		cout<<endl;
+		cout << endl;
 	}
-	cout<<endl;
+	cout << endl;
 }
 
-void Matrix::printRowSize(){
-	cout<<"Row = "<<getRow()<<endl;
+void Matrix::printRowSize()
+{
+	cout << "Row = " << getRow() << endl;
 }
 
-void Matrix::printColumnSize(){
-	cout<<"Column = "<<getColumn()<<endl;
+void Matrix::printColumnSize()
+{
+	cout << "Column = " << getColumn() << endl;
 }
 
-void Matrix::setTrans(){
-	Matrix *B = new Matrix; 
-	for(int i=0;i<getRow();i++){
-		vector <double> temp;
-		for(int j=0;j<getColumn();j++){
+void Matrix::setTrans()
+{
+	Matrix *B = new Matrix;
+	for (int i = 0; i < getRow(); i++)
+	{
+		vector<double> temp;
+		for (int j = 0; j < getColumn(); j++)
+		{
 			temp.push_back(data[j][i]);
 		}
 		B->data.push_back(temp);
 	}
-	trans=B;
+	trans = B;
 }
 
-void Matrix::printTrans(){
+void Matrix::printTrans()
+{
 	setTrans();
-	cout<<"transposition"<<endl;
+	cout << "transposition" << endl;
 	trans->print();
 }
 
-void Matrix::modify(){
-	cout<<"now"<<endl;
+void Matrix::modify()
+{
+	cout << "now" << endl;
 	print();
-	int i,j;
-	cout<<"input (i,j). (i,j) is element that needs modifying"<<endl;
-	cin>>i>>j;
+	int i, j;
+	cout << "input (i,j). (i,j) is element that needs modifying" << endl;
+	cin >> i >> j;
 	double alter;
-	cout<<"input the number you want to replace the element in ("<<i<<","<<j<<")"<<endl;
-	cin>>alter;
-	data[i-1][j-1]=alter;
-	cout<<"after modifying"<<endl;
+	cout << "input the number you want to replace the element in (" << i << "," << j << ")" << endl;
+	cin >> alter;
+	data[i - 1][j - 1] = alter;
+	cout << "after modifying" << endl;
 	print();
 }
 
-int Matrix::getColumn(){
+int Matrix::getColumn() const
+{
 	return data.size();
 }
 
-int Matrix::getRow(){
-	if(data.size()!=0){
+int Matrix::getRow() const
+{
+	if (data.size() != 0)
+	{
 		return data[0].size();
-	}else{
+	}
+	else
+	{
 		return 0;
 	}
 }
 
-vector<vector<double> > Matrix::getData(){
+vector<vector<double> > Matrix::getData() const
+{
 	return data;
 }
 
-Matrix Matrix::operator+(Matrix &A){
-	if(getColumn()==A.getColumn()&&getRow()==A.getRow()){
+Matrix Matrix::operator+(Matrix &A)
+{
+	if (getColumn() == A.getColumn() && getRow() == A.getRow())
+	{
 		Matrix *output = new Matrix;
-		for(int i=0;i<getColumn();i++){
+		for (int i = 0; i < getColumn(); i++)
+		{
 			vector<double> tmp;
-			for(int j=0;j<getRow();j++){
-				tmp.push_back(data[i][j]+A.data[i][j]);
+			for (int j = 0; j < getRow(); j++)
+			{
+				tmp.push_back(data[i][j] + A.data[i][j]);
 			}
 			output->data.push_back(tmp);
 		}
 		return *output;
-	}else{
-//		cout<<"error!"<<endl;
+	}
+	else
+	{
+		//		cout<<"error!"<<endl;
 		return *this;
 	}
 }
 
-Matrix Matrix::operator-(Matrix &A){
-	if(getColumn()==A.getColumn()&&getRow()==A.getRow()){
+Matrix Matrix::operator-(Matrix &A)
+{
+	if (getColumn() == A.getColumn() && getRow() == A.getRow())
+	{
 		Matrix *output = new Matrix;
-		for(int i=0;i<getColumn();i++){
+		for (int i = 0; i < getColumn(); i++)
+		{
 			vector<double> tmp;
-			for(int j=0;j<getRow();j++){
-				tmp.push_back(data[i][j]-A.data[i][j]);
+			for (int j = 0; j < getRow(); j++)
+			{
+				tmp.push_back(data[i][j] - A.data[i][j]);
 			}
 			output->data.push_back(tmp);
 		}
 		return *output;
-	}else{
-//		cout<<"error!"<<endl;
+	}
+	else
+	{
+		//		cout<<"error!"<<endl;
 		return *this;
 	}
 }
 
-Matrix Matrix::operator*(double k){
+Matrix Matrix::operator*(double k)
+{
 	Matrix *output = new Matrix;
-	for(int i=0;i<getColumn();i++){
+	for (int i = 0; i < getColumn(); i++)
+	{
 		vector<double> tmp;
-		for(int j=0;j<getRow();j++){
-			tmp.push_back(data[i][j]*k);
+		for (int j = 0; j < getRow(); j++)
+		{
+			tmp.push_back(data[i][j] * k);
 		}
 		output->data.push_back(tmp);
 	}
 	return *output;
 }
 
-Matrix Matrix::operator/(double k){
+Matrix Matrix::operator/(double k)
+{
 	Matrix *output = new Matrix;
-	for(int i=0;i<getColumn();i++){
+	for (int i = 0; i < getColumn(); i++)
+	{
 		vector<double> tmp;
-		for(int j=0;j<getRow();j++){
-			tmp.push_back(data[i][j]/k);
+		for (int j = 0; j < getRow(); j++)
+		{
+			tmp.push_back(data[i][j] / k);
 		}
 		output->data.push_back(tmp);
 	}
 	return *output;
 }
 
-Matrix Matrix::operator*(Matrix &A){
-	if(getRow()==A.getColumn()){
+Matrix Matrix::operator*(Matrix &A)
+{
+	if (getRow() == A.getColumn())
+	{
 		Matrix *output = new Matrix;
-		for(int i=0;i<getColumn();i++){
+		for (int i = 0; i < getColumn(); i++)
+		{
 			vector<double> tmp;
-			for(int j=0;j<A.getRow();j++){
-				double sub_sum=0;
-				for(int k=0;k<getRow();k++){
-					sub_sum+=data[i][k]*A.getData()[k][j];
+			for (int j = 0; j < A.getRow(); j++)
+			{
+				double sub_sum = 0;
+				for (int k = 0; k < getRow(); k++)
+				{
+					sub_sum += data[i][k] * A.getData()[k][j];
 				}
 				tmp.push_back(sub_sum);
 			}
 			output->data.push_back(tmp);
 		}
 		return *output;
-	}else{
-		cout<<"error!"<<endl;
+	}
+	else
+	{
+		cout << "error!" << endl;
 		return *this;
 	}
 }
 
-Matrix Matrix::operator=(Matrix &A){
-	data.clear();
-	for(int i=0;i<A.getColumn();i++){
-		vector<double> tmp;
-		for(int j=0;j<A.getRow();j++){
-			tmp.push_back(A.getData()[i][j]);
-		}
-		data.push_back(tmp);
-	}
-	return *this;
+Matrix& Matrix::operator=(const Matrix &A)
+{
+    if (this != &A) // Check for self-assignment
+    {
+        // Clear existing data
+        data.clear();
+        
+        // Copy data from A
+        for (int i = 0; i < A.getRow(); i++)
+        {
+            vector<double> tmp;
+            for (int j = 0; j < A.getColumn(); j++)
+            {
+                tmp.push_back(A.getData()[i][j]);
+            }
+            data.push_back(tmp);
+        }
+    }
+    return *this;
 }
 
-bool Matrix::operator==(Matrix &A){
-	if(getRow()!=A.getRow()||getColumn()!=A.getColumn()){
+bool Matrix::operator==(Matrix &A)
+{
+	if (getRow() != A.getRow() || getColumn() != A.getColumn())
+	{
 		return false;
-	}else{
-		bool test=true;
-		for(int i=0;i<getColumn();i++){
-			for(int j=0;j<getRow();j++){
-				if(data[i][j]-A.getData()[i][j]>pow(10,-10)){
+	}
+	else
+	{
+		bool test = true;
+		for (int i = 0; i < getColumn(); i++)
+		{
+			for (int j = 0; j < getRow(); j++)
+			{
+				if (data[i][j] - A.getData()[i][j] > pow(10, -10))
+				{
 					return false;
 				}
 			}
@@ -206,4 +269,3 @@ bool Matrix::operator==(Matrix &A){
 		return test;
 	}
 }
-
--- project3/src/squarematrix.cpp
+++ project3/src/squarematrix.cpp
@@ -1,147 +1,191 @@
-#include<iostream>
-#include<iomanip>
-#include<vector>
-#include<cmath>
-#include"matrix.h"
-#include"squarematrix.h"
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include "matrix.h"
+#include "squarematrix.h"
 using namespace std;
-SquareMatrix::SquareMatrix(int size){
-	cout<<"input the square matrix"<<endl;
-	for(int i=0;i<size;i++){
-		vector <double> temp;
-		for(int j=0;j<size;j++){
+SquareMatrix::SquareMatrix(int size)
+{
+	cout << "input the square matrix" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		vector<double> temp;
+		for (int j = 0; j < size; j++)
+		{
 			double c;
-			cin>>c;
+			cin >> c;
 			temp.push_back(c);
 		}
 		data.push_back(temp);
 	}
 }
 
-SquareMatrix::SquareMatrix(Matrix A){
-	if(A.getColumn()==A.getRow()){
-		data=A.getData();
-	}else{
-		cout<<"error";
+SquareMatrix::SquareMatrix(Matrix A)
+{
+	if (A.getColumn() == A.getRow())
+	{
+		data = A.getData();
+	}
+	else
+	{
+		cout << "error";
 	}
 }
 
-void SquareMatrix::setup(){
-	cout<<"input n in order to construct a n*n square matrix"<<endl;
+void SquareMatrix::setup()
+{
+	cout << "input n in order to construct a n*n square matrix" << endl;
 	int size;
-	cin>>size;
-	cout<<"input the matrix"<<endl;
-	for(int i=0;i<size;i++){
-		vector <double> temp;
-		for(int j=0;j<size;j++){
+	cin >> size;
+	cout << "input the matrix" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		vector<double> temp;
+		for (int j = 0; j < size; j++)
+		{
 			double a;
-			cin>>a;
+			cin >> a;
 			temp.push_back(a);
 		}
 		data.push_back(temp);
 	}
 }
 
-double Det(vector < vector <double> > input){
-	int n=input.size();
-	double output=0;
-	if(n==1){
+double Det(vector<vector<double> > input)
+{
+	int n = input.size();
+	double output = 0;
+	if (n == 1)
+	{
 		return input[0][0];
-	}else if(n==2){
-		return input[0][0]*input[1][1]-input[0][1]*input[1][0];
-	}else{
-		for(int k=0;k<n;k++){
-			vector < vector <double> >next;
-			for(int i=0;i<n;i++){
-				if(i!=k){
-					vector <double> tmp;
-					for(int j=1;j<n;j++){
+	}
+	else if (n == 2)
+	{
+		return input[0][0] * input[1][1] - input[0][1] * input[1][0];
+	}
+	else
+	{
+		for (int k = 0; k < n; k++)
+		{
+			vector<vector<double> > next;
+			for (int i = 0; i < n; i++)
+			{
+				if (i != k)
+				{
+					vector<double> tmp;
+					for (int j = 1; j < n; j++)
+					{
 						tmp.push_back(input[i][j]);
 					}
 					next.push_back(tmp);
 				}
 			}
-			if(k%2==0){
-				output+=input[k][0]*Det(next);
-			}else{
-				output-=input[k][0]*Det(next);
+			if (k % 2 == 0)
+			{
+				output += input[k][0] * Det(next);
+			}
+			else
+			{
+				output -= input[k][0] * Det(next);
 			}
 		}
 		return output;
 	}
 }
 
-void SquareMatrix::setDet(){
-	det=Det(data);
+void SquareMatrix::setDet()
+{
+	det = Det(data);
 }
 
-void SquareMatrix::printDeterminant(){
+void SquareMatrix::printDeterminant()
+{
 	setDet();
-	cout<<"determinant = "<<det<<endl;
+	cout << "determinant = " << det << endl;
 }
 
-void SquareMatrix::setAdjugate(){
-	SquareMatrix *C=new SquareMatrix;
-	for(int i=0;i<getRow();i++){
-		vector <double> c_temp;
-		for(int j=0;j<getRow();j++){
-			vector < vector <double> >next;
-			for(int p=0;p<getRow();p++){
-				if(p!=i){
-					vector <double> tmp;
-					for(int q=0;q<getRow();q++){
-						if(q!=j){
+void SquareMatrix::setAdjugate()
+{
+	SquareMatrix *C = new SquareMatrix;
+	for (int i = 0; i < getRow(); i++)
+	{
+		vector<double> c_temp;
+		for (int j = 0; j < getRow(); j++)
+		{
+			vector<vector<double> > next;
+			for (int p = 0; p < getRow(); p++)
+			{
+				if (p != i)
+				{
+					vector<double> tmp;
+					for (int q = 0; q < getRow(); q++)
+					{
+						if (q != j)
+						{
 							tmp.push_back(data[p][q]);
 						}
 					}
 					next.push_back(tmp);
 				}
 			}
-			if((i+j)%2==0){
+			if ((i + j) % 2 == 0)
+			{
 				c_temp.push_back(Det(next));
-			}else{
+			}
+			else
+			{
 				c_temp.push_back(-Det(next));
 			}
 		}
 		C->data.push_back(c_temp);
 	}
 	C->setTrans();
-	adjugate=(SquareMatrix*)(C->trans);
+	adjugate = (SquareMatrix *)(C->trans);
 }
 
-void SquareMatrix::printAdjugate(){
+void SquareMatrix::printAdjugate()
+{
 	setAdjugate();
-	cout<<"adjugate"<<endl;
+	cout << "adjugate" << endl;
 	adjugate->print();
 }
 
-void SquareMatrix::setInverse(){
+void SquareMatrix::setInverse()
+{
 	setDet();
 	setAdjugate();
-	if(det!=0){
+	if (det != 0)
+	{
 		SquareMatrix *temp = new SquareMatrix;
-		temp=adjugate;
-		for(int i=0;i<getRow();i++){
-			for(int j=0;j<getRow();j++){
-				temp->data[i][j]/=det;
+		temp = adjugate;
+		for (int i = 0; i < getRow(); i++)
+		{
+			for (int j = 0; j < getRow(); j++)
+			{
+				temp->data[i][j] /= det;
 			}
 		}
-		inverse=temp;
+		inverse = temp;
 	}
 }
 
-void SquareMatrix::printInverse(){
+void SquareMatrix::printInverse()
+{
 	setInverse();
-	if(det!=0){
-		cout<<"inverse"<<endl;
+	if (det != 0)
+	{
+		cout << "inverse" << endl;
 		inverse->print();
-	}else{
-		cout<<"inverse does not exist"<<endl;
+	}
+	else
+	{
+		cout << "inverse does not exist" << endl;
 	}
 }
 
-double SquareMatrix::getDet(){
+double SquareMatrix::getDet()
+{
 	setDet();
 	return det;
 }
-
